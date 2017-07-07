40809ba4cd2d8b4737669f0170cb11348020adf0
root <root@localhost.localdomain>
2017-07-07 17:27:48 +0300

    init

---
+++ mkweb.sh
@@ -0,0 +1,6 @@
+sudo mkdir -p /home/box/web/public
+sudo mkdir -p /home/box/web/public/img
+sudo mkdir -p /home/box/web/public/css
+sudo mkdir -p /home/box/web/public/js
+sudo mkdir -p /home/box/web/uploads
+sudo mkdir -p /home/box/web/etc
