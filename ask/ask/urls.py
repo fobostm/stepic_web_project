53a89f6e72dd5e410e272221077dc23a7c3315e4
root <root@localhost.localdomain>
2017-07-24 12:44:39 +0300

    signup

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', include('qa.urls')),
     url(r'^login/$', include('qa.urls')),
-    url(r'^signup$', include('qa.urls')),
+    url(r'^signup/$', include('qa.urls')),
     url(r'^question/123/$', include('qa.urls')),
     url(r'^ask/$', include('qa.urls')),
     url(r'^popular/$', include('qa.urls')),
--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+sudo pip install --upgrade django
+sudo pip install --upgrade gunicorn
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
